1522bf841cce3ff4d5794ee65007c8b1964296b6
rshvraj <33702025+rshvraj@users.noreply.github.com>
2022-08-15 09:32:06 +0530

    first commit

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "fa9037c827b9b0970f4c7be7e93a5465";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
